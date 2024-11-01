b65c2117b99b7bde3c123d772f2b22808a2771cd
karanvrm <142299124+karanvrm@users.noreply.github.com>
2024-11-01 14:49:11 +0530

    Add files via upload

---
+++ helper.h
@@ -0,0 +1,9 @@
+#ifndef HELPER_H  // Include guard to prevent multiple inclusion
+#define HELPER_H
+
+// Function prototypes for helper functions
+double calculate_jaccard_similarity(int common, int total1, int total2);
+int min(int a, int b);
+int max(int a, int b);
+
+#endif  // HELPER_H

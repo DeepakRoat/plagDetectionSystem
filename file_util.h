ed46bd3fd7058ca63a2e324e54ca7a37b65495ec
karanvrm <142299124+karanvrm@users.noreply.github.com>
2024-11-01 14:47:53 +0530

    Add files via upload

---
+++ file_util.h
@@ -0,0 +1,7 @@
+#ifndef FILE_UTIL_H  // Include guard to prevent multiple inclusions
+#define FILE_UTIL_H
+
+// Function to read the content of a file and return it as a dynamically allocated string
+char* read_file(const char *filename);
+
+#endif  // End of include guard
